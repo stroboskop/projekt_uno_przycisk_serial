b07f42c4934db61c8e0ea2409a8a29fdb843d579
stroboskop <kamil.ostrovsky@gmail.com>
2017-07-26 16:35:30 +0200

    stabilne
    kontrola, jednak klasa dla sterowania out

--- projekt_uno_przycisk_serial.ino
+++ projekt_uno_przycisk_serial.ino
@@ -1,9 +1,19 @@
-boolean stateOfInp = false;
+
 unsigned long prevMil = 0;
-const long inter = 50;
-bool prevPom = false;
-bool prevOsw = false;
-bool podworko = false;
+const long inter = 3000;
+
+boolean stateOfInp = false;
+boolean prevPom = false;
+boolean prevPod = false;
+boolean boolOutIO = false;
+boolean valIn = false;
+
+boolean pinPod = false;
+boolean pinPom = false;
+
+const int pod = 8;
+const int pom = 9;
+
 
 String inData = "";
 String finalString = "";
@@ -14,29 +24,15 @@ void setup() {
   pinMode(4, INPUT);
   pinMode(5, INPUT);
   pinMode(8, OUTPUT);
-
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  checkButtons();
+  checkButtonsNew();
   readSerial();
-  outputIO();
-}
-
-void outputIO(){
-  if(finalString == "pod" && inDataState == "1"){
-    podworko = true;
-  }
-  if(finalString == "pod" && inDataState == "0"){
-    podworko = false;
-  }
-
-  if(podworko == true){
-    digitalWrite(8, HIGH);
-  }
-  if(podworko == false){
-    digitalWrite(8, LOW);
-  }
+  
+  if(boolOutIO)
+    outputIO(finalString, inDataState);
 }
 
 
@@ -44,39 +40,74 @@ void readSerial(){
   if(Serial.available() >0){   
       char inChar = Serial.read();   
 
-      if(inChar == '\n'){
-        //Serial.println("i mamy stringa:  " + inData);
+      if(inChar == '\n' && valIn == true){       
         inData.trim();
-        finalString = String(inData[6]) + String(inData[7]) + String(inData[8]);
-        inDataState = inData[10];
-        //Serial.println("nasz final: " + finalString);
-        //Serial.println("nasz stan: " + inDataState);
+        finalString = String(inData[0]) + String(inData[1]) + String(inData[2]);
+        inDataState = inData[4];
         inData = "";
-         
-      }     
-      if(inChar != '\n' && inChar != '\r')
+        boolOutIO = true;
+        valIn = false;   
+      }      
+          
+      if(inChar != '\n' && inChar != '\r' && valIn == true)
         inData += inChar; 
+
+      if(inChar == '$'){
+      valIn = true;
+      }
 }
 }
 
-void checkButtons(){
+void outputIO(String loc, String outVal){
+    const int val = outVal.toInt();
+    if(loc == "pod"){
+    digitalWrite(pod, val);  
+    Serial.println(finalString + "=" + val);
+    finalString = "";
+    boolOutIO = false;
+    }
+
+    if(loc == "pom"){
+    digitalWrite(pom, val);  
+    Serial.println(finalString + "=" + val);
+    finalString = "";
+    boolOutIO = false;
+    }
+}
+
+
+void checkButtonsNew(){
   unsigned long curMil = millis();
   if(curMil - prevMil >= inter){
-    if(prevPom != digitalRead(5) || prevOsw != digitalRead(4)){
+    if(digitalRead(5) || digitalRead(4)){
       stateOfInp = true;   
     }
     prevMil = curMil;
   }
   
-  if(digitalRead(4) != prevOsw && stateOfInp == true){
-    Serial.println("pod," + String(digitalRead(4)));
-    prevOsw = digitalRead(4);
+  if(digitalRead(4) && stateOfInp == true){
+      if(prevPod == false){
+          finalString = "pod";
+          inDataState = "1";
+          boolOutIO = true;
+          prevPod = true;
+          Serial.println("false ");
+      }else if(prevPod == true){
+          finalString = "pod";
+          inDataState = "0";
+          boolOutIO = true;
+          prevPod = false;
+          Serial.println("true ");
+      }
+    Serial.println("i jestesmy ");
     stateOfInp = false;
   }
-  if(digitalRead(5) != prevPom && stateOfInp == true){
-    Serial.println("pom," + String(digitalRead(5)));
-    prevPom = digitalRead(5);
+  if(digitalRead(5) && stateOfInp == true){
+
+    //outputIO("pom", readPin);
+    Serial.println("i jestesmy");
     stateOfInp = false;
   }
 }
+
 
